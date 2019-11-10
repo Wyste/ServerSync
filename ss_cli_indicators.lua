34c935becbb2c056b3715d72e498974dac0ec947
Wyste <wysterine@gmail.com>
2019-11-10 10:13:59 -0500

    Closes #2

--- ss_cli_indicators.lua
+++ ss_cli_indicators.lua
@@ -24,11 +24,11 @@ if ss_enable_turn_signal_sync then
           if GetPedInVehicleSeat( vehicle, - 1 ) == GetPlayerPed( -1 ) then
             if pressedLeft then
               vehicleState.indicator.left = not vehicleState.indicator.left
-              TriggerServerEvent( "ServerSynd:SetVehicleIndicator", 1, vehicleState.indicator.left )
+              TriggerServerEvent( "ServerSync:SetVehicleIndicator", 1, vehicleState.indicator.left )
             end
             if pressedRight then
               vehicleState.indicator.right = not vehicleState.indicator.right
-              TriggerServerEvent( "ServerSynd:SetVehicleIndicator", 0, vehicleState.indicator.right )
+              TriggerServerEvent( "ServerSync:SetVehicleIndicator", 0, vehicleState.indicator.right )
             end
           end
         end
